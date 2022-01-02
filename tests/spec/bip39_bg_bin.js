a854ef50fafa63d44adf7547dae1ae485365be2e
pencaster <Krum@Haesli.net>
2022-01-02 01:48:31 +0100

    bin

---
+++ tests/spec/bip39_bg_bin.js
@@ -0,0 +1,14 @@
+module.exports = [
+    "coral", 
+    "divorce", 
+    "fault", 
+    "avocado", 
+    "benefit", 
+    "fitness", 
+    "bicycle", 
+    "nose", 
+    "program", 
+    "about", 
+    "trend", 
+    "length"
+]
