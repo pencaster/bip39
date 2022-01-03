afa03666fce08eecaf6e9ce13bada2b1c5e26c89
pencaster <Krum@Haesli.net>
2022-01-04 00:21:57 +0100

    gans

---
+++ tests/spec/bip39_12.js
@@ -0,0 +1,16 @@
+module.exports = [
+    "source",
+    "absent",
+    "abuse",
+    "copy",
+    "coral",
+    "again",
+    "core",
+    "idea",
+    "alert",
+    "gauge",
+    "corn",
+    "south"
+]
+
+
