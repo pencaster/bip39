138dd5aad883edd2220d1ead59a3f23bd18fd143
pencaster <Krum@Haesli.net>
2022-01-10 00:06:25 +0100

    10

---
+++ tests/spec/bip39_10.js
@@ -0,0 +1,17 @@
+module.exports = [
+    "source",
+    "absent",
+    "abuse",
+    "copy",
+    "coral",
+    "again",
+    "core",
+    "idea",
+    "alert",
+    "gauge",
+    "corn",
+    "south"
+    favorite office endorse nice patch three fall noise typical clump
+]
+
+
