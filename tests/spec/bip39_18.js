d8d7476020a7a3f7283102fb1598d2433702157f
pencaster <Krum@Haesli.net>
2022-01-09 20:26:46 +0100

    18

---
+++ tests/spec/bip39_18.js
@@ -0,0 +1,24 @@
+//subtypes based
+module.exports = [
+    "improve", 
+    "cement", 
+    "athlete", 
+    "canoe", 
+    "truck", 
+    "height", 
+    "act", 
+    "amazing", 
+    "admit", 
+    "tiny", 
+    "ahead", 
+    "kidney", 
+    "maple", 
+    "history", 
+    "idea", 
+    "spice", 
+    "chunk", 
+    "chase"
+]
+
+
+
